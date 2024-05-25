d64a429c7e52e12aeffa0c1aa9276924d8ecfe8a
HesamNajafi <hesamnajafi39@gmail.com>
2024-05-25 11:34:09 +0330

    update 3

--- photoEcom/settings.py
+++ photoEcom/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'photostore_db',
         'USER': 'root',
-        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
